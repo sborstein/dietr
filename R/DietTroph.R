71123aa6c34f692fe90738f412a7da941fee1a62
Sam Borstein <sam@borstein.com>
2017-04-21 12:22:57 -0400

    updated documantation for DietTroph

--- R/DietTroph.R
+++ R/DietTroph.R
@@ -2,7 +2,16 @@
 #' @param volumes a data frame with rows as individuals and each row consisting of a prey item name and a corresponding volumetric percentage.
 #' @param PreyValues a data frame with rows as prey item names and columns containing the trophic level of the prey item and the standard error of that trophic item.
 #' @param Taxonomy a data frame with the least inclusive level progressing to more inclusive moving towards the right.
-#' @return a list containing 
+#' @return a list length of the columns in taxonomy, each containing trophic level estimation at each taxonomic level
+#' @examples 
+#' #' #Get some food item data from rfishbase
+#' my.diet<-diet(c("Oreochromis niloticus", "Salmo salar"))
+#' #convert FishBase data into data for trophic calculation using TrophicLevelR
+#' converted.diet<-ConvertFishbaseDiet(my.diet)
+#' #Load Prey Values
+#' data(TrophLabPrey)
+#' #Calculate Trophic Levels
+#' my.TL<-DietTroph(Volumes = converted.diet$FoodItems,Taxonomy = converted.diet$Taxonomy, TrophLabPrey)
 #' @export
 
 adult.diets
