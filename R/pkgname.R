e943a75cf3a7abed68b3bcc5d41a23abfb1a4141
Sam Borstein <sam@borstein.com>
2018-04-04 16:18:11 -0400

    update package name info

---
+++ R/pkgname.R
@@ -0,0 +1,29 @@
+#' An R package to estimate trophic level from diet and food item data
+#'
+#' @description Trophic level estimation from diet and food item data
+#'
+#' @details 
+#' Package: tRophic
+#' 
+#' Type: Package
+#' 
+#' Title: An R package to estimate trophic level from diet and food item data
+#' 
+#' Version: 1.0a
+#' 
+#' Date: 2018-4-4
+#' 
+#' License: GPL (>= 2)
+#' 
+#' This package allows users to .
+#' 
+#' @author Samuel Borstein
+#' Maintainer: Samuel Borstein <sborstei@vols.utk.edu>
+#' 
+#' @seealso \code{\link{ConvertFishbaseDiet}},\code{\link{ConvertFishbaseFood}},\code{\link{DietTroph}},\code{\link{FoodTroph}},\code{\link{TrophLabPrey}},\code{\link{ChondrichthyesPrey}}
+#' 
+#' @docType package
+#' @name tRophic
+#"_PACKAGE"
+#> [1] "_PACKAGE"
+NULL
