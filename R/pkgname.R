9065455bee39729f41f1648248c8db414cc1893e
Sam Borstein <sam@borstein.com>
2018-04-04 16:24:09 -0400

    Updated package info

--- R/pkgname.R
+++ R/pkgname.R
@@ -15,8 +15,13 @@
 #' 
 #' License: GPL (>= 2)
 #' 
-#' This package allows users to .
-#' 
+#' This package allows users to calculte trophic levels from proportional diet data or from food 
+#' items given trophic levels of the prey items. This package calculates trophic level following 
+#' the procedures from TrophLab, which was a microsoft access program. This implementation is faster
+#' than the original microsoft access program and also allows for a hierarchical estimation of 
+#' trophic level given a corresponding "taxonomy" data frame (i.e. estimate trophic level for a 
+#' individual,population,species, etc.). 
+#'  
 #' @author Samuel Borstein
 #' Maintainer: Samuel Borstein <sborstei@vols.utk.edu>
 #' 
